74e55db65089ebce5d4d753de11b3f27d16d3dc8
Rodrigo Ramos <rodrigoramosrs2@hotmail.com>
2021-11-09 16:32:24 -0300

    Update shrink-wsl-vhdx-size.ps1

--- scripts/shrink-wsl-vhdx-size.ps1
+++ scripts/shrink-wsl-vhdx-size.ps1
@@ -9,15 +9,15 @@ $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
 if($hyperv.State -eq "Enabled") {
     Write-Host "STOPING SERVICES ..."
 
-	net stop com.docker.service
-	taskkill /IM "docker.exe" /F
-	taskkill /IM "Docker Desktop.exe" /F
-	wsl --shutdown
-	
-	Write-Host "OPTIMIZING WSL SIZE ..."
-	Optimize-VHD -Path "$($env:LOCALAPPDATA)\Docker\wsl\data\ext4.vhdx" -Mode Full
-	Write-Host "DONE!"
+    net stop com.docker.service
+    taskkill /IM "docker.exe" /F
+    taskkill /IM "Docker Desktop.exe" /F
+    wsl --shutdown
+
+    Write-Host "OPTIMIZING WSL SIZE ..."
+    Optimize-VHD -Path "$($env:LOCALAPPDATA)\Docker\wsl\data\ext4.vhdx" -Mode Full
+    Write-Host "DONE!"
 } else {
     Write-Host "Hyper-V is disabled. To reduce WSL image size you need to install Hyper-V"
-	Write-Host "command: Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All"
+    Write-Host "command: Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All"
 }
